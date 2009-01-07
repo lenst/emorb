a8ed17055160fd18dbdc1f912fad4925e787ebc0
Lennart Staflin <lstaflin@gmail.com>
2009-01-07 14:36:21 +0100

    added some non dist files

---
+++ loaddefs.el
@@ -0,0 +1,12 @@
+;;;; loaddefs  -- genereated autoloads
+
+;;;### (autoloads (corba-init) "corba" "corba.el" (18566 63655))
+;;; Generated autoloads from corba.el
+
+(autoload 'corba-init "corba" "\
+Not documented
+
+\(fn &optional ARGS)" nil nil)
+
+;;;***
+
